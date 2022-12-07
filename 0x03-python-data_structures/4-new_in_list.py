3880d7a5f8ec5ff2abf9b6a1afb69f140be26929
Smart-Tech13 <festusbaylin@gmail.com>
2022-12-06 20:12:05 -0800

    Write a function that replaces an element in a list at a specific position without modifying the original list (like in C).

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        for item in my_list[::-1]:
-            print("{:d}".format(item))
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+    return new_list
